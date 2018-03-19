b0916cf7f7e9fcd9e3be95e375e720307175a09c
WebTarantul <tarstar1990@gmail.com>
2018-03-19 11:07:18 +0200

    top-line finish

--- decormetal.net.ua_WM/app/js/common.js
+++ decormetal.net.ua_WM/app/js/common.js
@@ -1,5 +1,22 @@
 $(function() {
 
-	// Custom JS
+      $("#my-menu").mmenu({
+      
+          extensions: [
+          "fx-listitems-drop",
+          "pagedim-white",
+          "fx-menu-slide"
+          ],
+          navbar: {
+    title: '<span class="logo__img--favicon">D</span>'
+  }
+      });
+      var API = $("#my-menu").data( "mmenu" );
+      API.bind('open', function(){
+      	$('.hamburger').addClass('is-active');
+      }).bind('closed', function(){
+      	$('.hamburger').removeClass('is-active');
+      	$('.main-nav__item.mm-selected').removeClass('mm-selected');
+      })
 
 });
--- decormetal.net.ua_WM/gulpfile.js
+++ decormetal.net.ua_WM/gulpfile.js
@@ -35,6 +35,7 @@ gulp.task('sass', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/mmenu/js/jquery.mmenu.all.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
