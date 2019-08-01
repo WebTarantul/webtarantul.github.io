ad604d8e65a2a6acb58c22005656115f9daa7381
WebTarantul <tarstar1990@gmail.com>
2019-08-01 14:30:02 +0300

    repair pathes of starDB

--- star-db/service-worker.js
+++ star-db/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.100c1f0bb5d91bfc0716c255b0e53520.js"
+  "./precache-manifest.abde23f6c0a667288f868cf9829860b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
