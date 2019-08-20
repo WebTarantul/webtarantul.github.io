feb367cf5c5e00cef653ba76476d2d7cbd8f43a6
WebTarantul <tarstar1990@gmail.com>
2019-08-20 14:56:00 +0300

    change re-store files

---
+++ re-store/precache-manifest.ac8e9ab002fa0b08e227768a09250a23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69733e3be9e126b753aaed14d4603b1",
+    "url": "/re-store/index.html"
+  },
+  {
+    "revision": "22695e65a297fd7ea83c",
+    "url": "/re-store/static/css/main.d9ffc781.chunk.css"
+  },
+  {
+    "revision": "69a972a63860547b6fb1",
+    "url": "/re-store/static/js/2.926551b6.chunk.js"
+  },
+  {
+    "revision": "22695e65a297fd7ea83c",
+    "url": "/re-store/static/js/main.0f309041.chunk.js"
+  },
+  {
+    "revision": "5d6fe284d288fb554198",
+    "url": "/re-store/static/js/runtime~main.d45e52b3.js"
+  },
+  {
+    "revision": "72ea08ca00a2459b771069d5e41e9bde",
+    "url": "/re-store/static/media/cloud.72ea08ca.svg"
+  },
+  {
+    "revision": "90680f8be0e91bc9d74f5544b47b192b",
+    "url": "/re-store/static/media/spinner.90680f8b.svg"
+  }
+]);
