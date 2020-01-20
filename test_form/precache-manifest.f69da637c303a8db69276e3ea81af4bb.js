be2af4539ec36b1d37b4e2e578bb7e85a2690ef0
WebTarantul <tarstar1990@gmail.com>
2020-01-20 22:36:41 +0200

    add test task

---
+++ test_form/precache-manifest.f69da637c303a8db69276e3ea81af4bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee33196f440224479ad2a08141faf26d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "687a03f529ded4aeb1c1",
+    "url": "/static/css/main.9c825ffe.chunk.css"
+  },
+  {
+    "revision": "8d544915cc97e5eb8bfe",
+    "url": "/static/js/2.808c52bf.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.808c52bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "687a03f529ded4aeb1c1",
+    "url": "/static/js/main.9dbeb7cc.chunk.js"
+  },
+  {
+    "revision": "aca4b1192543df87397f",
+    "url": "/static/js/runtime-main.2a49fc56.js"
+  }
+]);
